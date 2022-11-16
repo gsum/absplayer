4e34e6f648096a07278d932e3e59b0d6553a7b23
Nik Jenson <nik@biolexistx.com>
2022-11-16 00:16:01 -0700

    Completed piece1.abc and began piece2.abc. The stubs of both tests have been added to SequencePlayerTest

--- test/abc/sound/SequencePlayerTest.java
+++ test/abc/sound/SequencePlayerTest.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiUnavailableException;
+
 public class SequencePlayerTest {
 
     // TODO: warmup #2
@@ -11,6 +14,30 @@ public class SequencePlayerTest {
     @Test
     public void test() {
         fail("Not yet implemented");
+    }
+
+    @Test
+    public void testPieceOne(){
+        try {
+            SequencePlayer player = new SequencePlayer(140, 12);
+
+        } catch (MidiUnavailableException e) {
+            throw new RuntimeException(e);
+        } catch (InvalidMidiDataException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void testPieceTwo(){
+        try {
+            SequencePlayer player = new SequencePlayer(200, 12);
+
+        } catch (MidiUnavailableException e) {
+            throw new RuntimeException(e);
+        } catch (InvalidMidiDataException e) {
+            throw new RuntimeException(e);
+        }
     }
     
 
