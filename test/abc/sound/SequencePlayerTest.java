b1ad9c61097a2fbcd7f59e9920fb94c4d176f6d0
Suman Koirala <sumanrakoirala@gmail.com>
2022-11-20 22:05:12 -0600

    test cases added

--- test/abc/sound/SequencePlayerTest.java
+++ test/abc/sound/SequencePlayerTest.java
@@ -2,16 +2,62 @@ package abc.sound;
 
 import static org.junit.Assert.*;
 
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiUnavailableException;
+
 import org.junit.Test;
 
 public class SequencePlayerTest {
 
-    // TODO: warmup #2
     
     @Test
-    public void test() {
-        fail("Not yet implemented");
+    public void piece1Test() {
+        try {
+            SequencePlayer player = new SequencePlayer(120, 2);
+
+            assertEquals(1,1);
+            assertEquals(1,1);
+
+            player.addNote(new Pitch('C').toMidiNote(), 0, 1);
+            player.addNote(new Pitch('C').transpose(Pitch.OCTAVE).toMidiNote(), 7, 1);
+
+            System.out.println(player);
+
+            player.play();
+
+
+        } catch (MidiUnavailableException mue) {
+            fail("MidiUnavailableException");
+            mue.printStackTrace();
+        } catch (InvalidMidiDataException imde) {
+            fail("InvalidMidiDataException");
+            imde.printStackTrace();
+        }
     }
     
+    @Test
+    public void piece2Test() {
+        try {
+            SequencePlayer player = new SequencePlayer(120, 2);
+
+            assertEquals(1,1);
+            assertEquals(1,1);
+
+            player.addNote(new Pitch('C').toMidiNote(), 0, 1);
+            player.addNote(new Pitch('C').transpose(Pitch.OCTAVE).toMidiNote(), 7, 1);
+
+            System.out.println(player);
+
+            player.play();
+
+
+        } catch (MidiUnavailableException mue) {
+            fail("MidiUnavailableException");
+            mue.printStackTrace();
+        } catch (InvalidMidiDataException imde) {
+            fail("InvalidMidiDataException");
+            imde.printStackTrace();
+        }
+    }
 
 }
